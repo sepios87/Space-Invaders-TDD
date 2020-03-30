686392e4ac980fae945f8f0b87a1ad62ea99ff8d
Florian Toribio <florian.toribio@gmail.com>
2020-03-30 17:16:11 +0200

    V1

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/SpaceInvaders.java
@@ -0,0 +1,25 @@
+    package fr.unilim.iut.spaceinvaders;
+  
+    public class SpaceInvaders {
+
+	    int longueur;
+	    int hauteur;
+
+	    public SpaceInvaders(int longueur, int hauteur) {
+		   this.longueur = longueur;
+		   this.hauteur = hauteur;
+	   }
+	    
+		@Override
+		public String toString() {
+			StringBuilder espaceDeJeu = new StringBuilder();
+			for (int i = 0; i < hauteur; i++) {
+				for (int j = 0; j < longueur; j++) {
+					espaceDeJeu.append('.');
+				}
+				espaceDeJeu.append('\n');
+			}
+			return espaceDeJeu.toString();
+		}
+
+   }
---
+++ src/test/java/fr/unilim/iut/spaceinvaders/SpaceInvadersTest.java
@@ -0,0 +1,24 @@
+    package fr.unilim.iut.spaceinvaders;
+
+    import static org.junit.Assert.assertEquals;
+    import org.junit.Test;
+
+    public class SpaceInvadersTest {
+	
+	   @Test
+	   public void test_AuDebut_JeuSpaceInvaderEstVide() {
+		    SpaceInvaders spaceinvaders = new SpaceInvaders(15, 10);
+		    assertEquals("" + 
+		    "...............\n" + 
+		    "...............\n" +
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" + 
+		    "...............\n" , spaceinvaders.toString());
+	        }
+
+       }
