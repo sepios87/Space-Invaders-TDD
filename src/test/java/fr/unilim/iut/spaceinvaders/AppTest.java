52ffaff40cc2f4d82c134619d2ec943114f6f70c
Florian Toribio <florian.toribio@gmail.com>
2020-03-30 16:54:27 +0200

    v0

---
+++ src/test/java/fr/unilim/iut/spaceinvaders/AppTest.java
@@ -0,0 +1,20 @@
+package fr.unilim.iut.spaceinvaders;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
